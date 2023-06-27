27b327e845239b868a72bb33fd47c7137e9e2ee0
MARVIN <o.silverygold@gmail.com>
2023-06-27 16:09:23 +0300

    Add modal markup container and event listeners

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,23 +1,30 @@
-import getShows from './api.js';
-
+import { getShows } from './api.js';
+import showModal from './handlePopup.js';
 const movieContainer = document.querySelector('.movie-list');
 
 // displayshows function
 const displayShows = () => {
   getShows().then((data) => {
-    const shows = data.map((show) => `
-          <div class="movie-card">
+    data.splice(1, 230);
+    const shows = data.map((show, index) =>
+    `
+          <div class="movie-card" data-id="${data[index]['externals']['thetvdb']}">
               <img src="${show.image.medium}" alt="${show.name}">
               <h3 class="movie-title">${show.name}</h3>
               <div class="interactions">
-                  <button class="comments">comment</button>
+                  <button class="comments" click="details">comment</button>
                   <i class="like fa-regular fa-heart"></i>
               </div>
-          </div>
+            <div class="my-modal">
+            </div>
+         </div>
           `).join('');
 
     movieContainer.innerHTML = shows;
+    movieContainer.addEventListener('click', e => showModal(e));
   });
+
+
 };
 
 export default displayShows;
