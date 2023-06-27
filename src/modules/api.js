af6ff147254b4f89082a6e348161db519b15417d
MARVIN <o.silverygold@gmail.com>
2023-06-27 16:11:38 +0300

    Add functionality to get single show details

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,3 +1,12 @@
-const getShows = () => fetch('https://api.tvmaze.com/shows').then((res) => res.json());
+const getShows = () => fetch('https://api.tvmaze.com/shows').then((res) => 
+res.json());
 
-export default getShows;
+const getShowDetails = async (id) => {
+    const baseUrl = 'https://api.tvmaze.com/lookup/shows?thetvdb=';
+    const detailUrl = `${baseUrl + id}`;
+    const response = await fetch(detailUrl).then(response => response.json());
+    console.log(response);
+    return response;
+} 
+
+export { getShows, getShowDetails };
