0a69839a5d1e909bd2520ec5d76d8de1a7b17909
MARVIN <o.silverygold@gmail.com>
2023-06-29 09:43:34 +0300

    Set up jest test environment

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,13 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
     ],
   },
 };
